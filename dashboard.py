64568b6d9b6e5e2a9d155ced4ddf03ecd2c58e1c
Strategy Ace LLC <jared@strategyace.win>
2024-12-21 22:03:02 -0500

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+
+def main():
+  st.title("TBD Mayoral Race")
+  # Input the future date (format: YYYY-MM-DD)
+  future_date_str = "2024-12-31"
+  future_date = datetime.strptime(future_date_str, "%Y-%m-%d")
+
+  # Get today's date
+  today = datetime.today()
+  
+  # Calculate the difference in days
+  days_until = (future_date - today).days
+  
+  st.subheader(f"{days_until} days until Election Day.")
+    
+
+if __name__ == "__main__":
+    main()
