963a20a7b8bc446ccc0107abb17a690782ed9c1c
Strategy Ace LLC <jared@strategyace.win>
2024-12-22 21:18:08 -0500

    Update Main_Dashboard.py

--- Main_Dashboard.py
+++ Main_Dashboard.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
+st.set_page_config(page_title="Manager App", page_icon=":ballot_box_with_ballot:")
 
 def main():
   col1,col2 = st.columns(2)
