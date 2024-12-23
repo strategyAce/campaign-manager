6a37c3e95b40497c64ed0a146ebbb9b1776ddbb4
Strategy Ace LLC <jared@strategyace.win>
2024-12-22 21:20:35 -0500

    Update 1_Win_Analysis.py

--- pages/1_Win_Analysis.py
+++ pages/1_Win_Analysis.py
@@ -44,7 +44,7 @@ def main():
         st.title("Campaign Win Analysis Tool")
     with col2:
         st.image("Campaign-Predictor.png",width=150)
-    st.subheader("Run predictions that can give your campaign an edge up on the competition")
+    st.subheader("Run predictions that can give your campaign winning insight")
     st.write("This is a product of Strategy Ace LLC")
     st.write("Version: BETAv0.1...date:09/29/2024")
     st.divider()
