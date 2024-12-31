d734e1d1467c08af0a8f60a89383cbdcabd5a00a
Strategy Ace LLC <jared@strategyace.win>
2024-12-30 21:10:13 -0500

    Update 2_Maps.py

--- pages/2_Maps.py
+++ pages/2_Maps.py
@@ -51,7 +51,7 @@ def main():
 
           map.on('load', () => {
               // Add a click event for the existing data-driven-circles layer
-              map.on('click', 'precincts-20240415-shapefile-4g4p5r', (e) => {
+              map.on('click', 'data-driven-circle', (e) => {
                   const coordinates = e.features[0].geometry.coordinates.slice();
                   const properties = e.features[0].properties;
 
@@ -66,12 +66,12 @@ def main():
               });
 
               // Change the cursor to a pointer when over the layer
-              map.on('mouseenter', 'precincts-20240415-shapefile-4g4p5r', () => {
+              map.on('mouseenter', 'data-driven-circle', () => {
                   map.getCanvas().style.cursor = 'pointer';
               });
 
               // Change it back when it leaves
-              map.on('mouseleave', 'precincts-20240415-shapefile-4g4p5r', () => {
+              map.on('mouseleave', 'data-driven-circle', () => {
                   map.getCanvas().style.cursor = '';
               });
           });
