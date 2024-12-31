eb90194f45b183521a5107c175a3519c0bad2e6e
Strategy Ace LLC <jared@strategyace.win>
2024-12-30 21:38:21 -0500

    Update 2_Maps.py

--- pages/2_Maps.py
+++ pages/2_Maps.py
@@ -58,8 +58,7 @@ def main():
                   new mapboxgl.Popup()
                       .setLngLat(coordinates)
                       .setHTML(
-                          `<h3>Precinct: ${properties['PRECINCT']}</h3>` +
-                          '<p>Democrat Percentage: ${properties['PCT-DEM']}</p>`
+                          `<h3>Precinct: ${properties['PRECINCT']}</h3>`
                       )
                       .addTo(map);
               });
