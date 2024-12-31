ab789e739e01c43b7df5295866891d10c252754c
Strategy Ace LLC <jared@strategyace.win>
2024-12-31 12:28:43 -0500

    Update Main_Dashboard.py

--- Main_Dashboard.py
+++ Main_Dashboard.py
@@ -49,7 +49,7 @@ def main():
       st.dataframe(df, hide_index=True)
     else:
       st.warning("No Voter Universe File Found")
-    st.write("Definitions: \n Hot=Last 2 Gen & last 2 Prim, Warmer=Last 2 Gen & last Prim, Warm=Last 2 Gen, Infreq=At least 1 vote in either last 2 Gen or 2 Prim")
+    st.write("Definitions: Hot= *Last 2 Gen & last 2 Prim*, Warmer= *Last 2 Gen & last Prim*, Warm= *Last 2 Gen*, Infreq= *At least 1 vote in either last 2 Gen or 2 Prim*")
     st.write("*Data updated on 12/22/2024*")
   
   #Campaign Map
