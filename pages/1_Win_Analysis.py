ada6579d3bcdc8599f633815203e333a4540fc5a
Strategy Ace LLC <jared@strategyace.win>
2025-01-02 21:30:06 -0500

    Update 1_Win_Analysis.py

--- pages/1_Win_Analysis.py
+++ pages/1_Win_Analysis.py
@@ -32,8 +32,8 @@ def main():
   st.write("")
   st.write("")
   buffer = st.slider("Select the percentage buffer you would like to add", 0.0,5.0,(2.0,3.0))
-  votegoalL = PredVoters*((50+buffer[0])/100)
-  votegoalH = PredVoters*((50+buffer[1])/100)
+  votegoalL = np.ceil(PredVoters*((50+buffer[0])/100))
+  votegoalH = np.ceil(PredVoters*((50+buffer[1])/100))
   st.subheader(f"Your vote goal range with the added buffer comes to:")
   st.title(f"{votegoalL} - {votegoalH}")
   
