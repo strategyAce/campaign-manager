3486def6e3c9a913b0718de3f2847e677723f264
Strategy Ace LLC <jared@strategyace.win>
2025-01-03 15:16:54 -0500

    Update 2_Maps.py

--- pages/2_Maps.py
+++ pages/2_Maps.py
@@ -176,6 +176,8 @@ def main():
   
   fig.update_layout(mapbox_style="carto-positron")
   st.plotly_chart(fig)
+  feature_count = len(gdf['features'])
+  st.write(f"Number of features: {feature_count}")
   
   
   #App Footer
