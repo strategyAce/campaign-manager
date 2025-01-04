f473ae4975fc59e46773e4c4b0deb7f137d6734f
Strategy Ace LLC <jared@strategyace.win>
2025-01-03 21:25:53 -0500

    Update 3_Precinct_Analysis.py

--- pages/3_Precinct_Analysis.py
+++ pages/3_Precinct_Analysis.py
@@ -75,10 +75,6 @@ def main():
                <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
                <link href="https://api.mapbox.com/mapbox-gl-js/v3.8.0/mapbox-gl.css" rel="stylesheet">
                <script src="https://api.mapbox.com/mapbox-gl-js/v3.8.0/mapbox-gl.js"></script>
-               <style>
-                   body { margin: 0; padding: 0; }
-                   #map { position: absolute; top: 0; bottom: 0; width: 100%; }
-               </style>
            </head>
            <body>
                <div id="map"></div>
