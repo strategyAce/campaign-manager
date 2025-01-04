b70db59593f5b935c0a077e4d525bcc3765da7f3
Strategy Ace LLC <jared@strategyace.win>
2025-01-03 21:36:05 -0500

    Update 3_Precinct_Analysis.py

--- pages/3_Precinct_Analysis.py
+++ pages/3_Precinct_Analysis.py
@@ -58,8 +58,8 @@ def main():
           st.subheader("")
           st.subheader(f"Here is the data for precinct :blue[{selPrecinct}]")
           demogData = precinctData[precinctData['PRECINCT'] == selPrecinct]
-          lat = demogData['LATITUDE'].values[0]
-          long = demogData['LONGITUDE'].values[0]
+          lat = demogData['LATITUDE']
+          long = demogData['LONGITUDE']
           st.write(demogData)
           st.subheader("")
 
